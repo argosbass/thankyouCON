a78f4c89f60b3d611fe0a3144dd0ea4a1a38aea2
argosbass <argosbass@hotmail.com>
2015-02-23 22:24:26 -0600

    Thank You Files

---
+++ js/contact.js
@@ -0,0 +1,37 @@
+$(function()
+{
+    $("#about form").submit(function()
+    {
+       
+        var form = $(this);
+        var str = form.serialize();
+        alert(str);
+        $.ajax(
+        {
+            type: "POST",
+            url: "contact.php",
+            data: str,
+            success: function(msg)
+            {
+                $("#about .result .alert").remove();
+                msg = JSON.parse(msg);
+
+                if(msg.status == 'OK')
+                {
+                    $('#about .result').append('<div class="alert alert-success">Your message has been sent. Thank you!</div');
+                }
+                else if(msg.text)
+                {
+                    $.each(msg.text, function(i, elem){
+                        $('#about .result').append('<div class="alert alert-error">' + elem + '</div');
+                    })
+                }
+                else
+                {
+                    $('#about .result').append('<div class="alert alert-error">Error</div');
+                }
+            }
+        })
+        return false;
+    })
+})
